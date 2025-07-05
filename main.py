5e2dd336be55ab56956248c030c38d7bf17ca88a
Anchal malik <118867430+anchal405@users.noreply.github.com>
2025-07-05 20:03:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import joblib
 import numpy as np
 import pandas as pd
 
-# Load trained model
+
 model = joblib.load("models/price_model.joblib")
 
-# FastAPI app
+
 app = FastAPI(title="HaritSetu AI Price Predictor with Suggestions")
 
-# Input schema
+
 class CropInput(BaseModel):
     crop_type: str
     residue_type: str
@@ -19,7 +19,7 @@ class CropInput(BaseModel):
     month: int
     quantity_kg: int
 
-# 🔥 Rule-based suggestion function
+
 def suggest_use(residue_type: str):
     suggestions = {
         "straw": "biofuel or composting",
@@ -33,10 +33,10 @@ def suggest_use(residue_type: str):
     }
     return suggestions.get(residue_type.lower(), "general composting")
 
-# POST endpoint
+
 @app.post("/predict_price")
 def predict_price(data: CropInput):
-    # Prepare input
+   
     input_df = pd.DataFrame([{
         "crop_type": data.crop_type,
         "residue_type": data.residue_type,
@@ -46,11 +46,11 @@ def predict_price(data: CropInput):
         "quantity_kg": data.quantity_kg
     }])
 
-    # Predict price (log → exp)
+   
     log_price = model.predict(input_df)[0]
     final_price = round(float(np.exp(log_price)), 2)
 
-    # Suggest best use
+   
     suggestion = suggest_use(data.residue_type)
 
     return {
