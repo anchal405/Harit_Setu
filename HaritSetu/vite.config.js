78226dbd1489581e853c4d22abdce17d86412670
Shanti Kumari <144619700+ShantiKumariGautam@users.noreply.github.com>
2025-07-13 04:41:32 +0530

    fix: added vercel config and cleaned rollupOptions for Vercel build

--- HaritSetu/vite.config.js
+++ HaritSetu/vite.config.js
@@ -3,4 +3,23 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  
+  build: {
+    rollupOptions: {
+      external: [
+        'firebase/app',
+        'firebase/auth',
+        'firebase/firestore',
+        'firebase/storage',
+        'firebase/functions',
+        'firebase/database',
+        'firebase/messaging',
+        'firebase/analytics',
+        'firebase/performance',
+        'firebase/remote-config',
+        'firebase/installations',
+        
+      ],
+    },
+  },
 });
